4a07393003185ca45f5d888965e09a047aa9157a
3isking <acebrendan07@gmail.com>
2026-01-30 14:28:59 +0800

    Managers Initialized in GameMaster

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/GameMaster.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/GameMaster.java
@@ -1,8 +1,24 @@
 package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+import io.github.some_example_name.lwjgl3.managers.CollisionManager;
+import io.github.some_example_name.lwjgl3.managers.EntityManager;
+import io.github.some_example_name.lwjgl3.managers.InputManager;
+import io.github.some_example_name.lwjgl3.managers.MovementManager;
+import io.github.some_example_name.lwjgl3.managers.OutputManager;
+import io.github.some_example_name.lwjgl3.managers.SceneManager;
 
 public class GameMaster extends ApplicationAdapter{
+    private OrthographicCamera camera;
+    private SceneManager sceneManager;
+    private EntityManager entityManager;
+    private MovementManager movementManager;
+    private CollisionManager collisionManager;
+    private InputManager inputManager;
+    private OutputManager outputManager;
+
     public void create(){
 
     }
@@ -12,6 +28,10 @@ public class GameMaster extends ApplicationAdapter{
     }
 
     public void dispose(){
+
+    }
+
+    public void cameraControl(){
 
     }
 }
