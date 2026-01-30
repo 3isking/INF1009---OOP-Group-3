fbada2130206d03d9b5a06ba5a39f2b2f29876db
chiauyee <wongchiauyee@gmail.com>
2026-01-30 15:41:59 +0800

    iCollidable update

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/entities/iCollidable.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/entities/iCollidable.java
@@ -1,7 +1,10 @@
 package io.github.some_example_name.lwjgl3.entities;
 
 public interface iCollidable {
-	BoundingBox getBoundingbox();
+	public BoundingBox getBoundingBox();
+
+	public void setBoundingBox(BoundingBox boudingBox);
 	
+	public void onCollision(iCollidable other);
 	
 }
