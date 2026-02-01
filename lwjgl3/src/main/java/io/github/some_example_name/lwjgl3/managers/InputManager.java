52a56115200c4c1cffd702a91a6ae0560d80abb3
cgxh <cgxh125@yahoo.com.sg>
2026-02-02 02:26:35 +0800

    Input Manager

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/managers/InputManager.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/managers/InputManager.java
@@ -1,5 +1,109 @@
 package io.github.some_example_name.lwjgl3.managers;
 
-public class InputManager {
+import java.util.HashMap;
+import java.util.Map;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector2;
+
+public class InputManager{
+    private final Map<String, Binding> bindings = new HashMap<>();
+    private final Map<String, Boolean> lastState = new HashMap<>();
+    private String waitingForBind = null;
     
+    private enum Device {
+        KEYBOARD,
+        MOUSE
+    }
+    
+    private static class Binding {
+        Device device;
+        int code;
+
+        Binding(Device device, int code) {
+            this.device = device;
+            this.code = code;
+        }
+    }
+    
+	private boolean isPressed(Binding b) {
+    	if (b.device == Device.KEYBOARD) {
+    	    return Gdx.input.isKeyPressed(b.code);
+    	} 
+    	else { //It is a mouse
+    	    return Gdx.input.isButtonPressed(b.code);
+    	}
+    }
+
+	InputManager() {
+		bind("up", Device.KEYBOARD, Input.Keys.W);
+    	bind("down", Device.KEYBOARD, Input.Keys.S);
+    	bind("left", Device.KEYBOARD, Input.Keys.A);
+    	bind("right", Device.KEYBOARD, Input.Keys.D);
+    	bind("jump", Device.KEYBOARD, Input.Keys.SPACE);
+    	bind("action", Device.MOUSE, Input.Buttons.LEFT);
+	}
+	
+	//add to the bindings and laststate hashmap
+	private void bind(String action, Device device, int code) {
+        bindings.put(action, new Binding(device, code));
+        lastState.put(action, false);
+    }
+
+
+	//get keypressed for keyboard and mouse
+	public boolean InputPressed(String action) {
+        Binding b = bindings.get(action);
+        //This will get the Binding that is mapped to the string action
+        // After the above code, b.device == Device.KEYBOARD
+        //b.code == Input.Keys.SPACE
+        if (b == null) {
+        	return false; // If it has no Binding mapped to the action
+        }
+
+        boolean pressed = isPressed(b); //pressed == the inputkey
+        boolean wasPressed = lastState.getOrDefault(action, false);
+        lastState.put(action, pressed);
+
+        return pressed && !wasPressed; //returns true if a key is pressed the first time
+    }
+	
+	 public boolean InputHeld(String action) {
+		 Binding b = bindings.get(action);
+	     if (b == null) {
+	    	 return false;
+	     }
+	     return isPressed(b);
+	 }
+	
+	 public Vector2 getMousePosition() {
+		 return new Vector2(Gdx.input.getX(), Gdx.input.getY());
+	}
+	 
+	 public void setKeyBind(String action) {
+		waitingForBind = action;
+	}
+	
+	
+	//wait to set keybind for keyboard
+	 private boolean keyDown(int keycode) {
+        if (waitingForBind != null) {
+        	bindings.put(waitingForBind, new Binding(Device.KEYBOARD, keycode));
+            lastState.put(waitingForBind, false);
+            waitingForBind = null;
+            return true;
+        }
+        return false;
+    }
+
+	 //wait to set keybind for mouse
+	 private boolean touchDown(int x, int y, int pointer, int button) {
+		 if (waitingForBind != null) {
+			 bindings.put(waitingForBind, new Binding(Device.MOUSE, button));
+             lastState.put(waitingForBind, false);
+             waitingForBind = null;
+             return true;
+        }
+        return false;
+    } 
 }
